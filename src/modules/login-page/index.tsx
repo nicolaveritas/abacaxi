9c2a3202c540901df966ed73c2b270b445acba5e
Nicola Verità <nicola.veritas@gmail.com>
2021-01-17 19:26:57 +0100

    change: use isAuthenticated to redirect from loginPage

--- src/modules/login-page/index.tsx
+++ src/modules/login-page/index.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import LoginButton from "../auth/LoginButton";
 import { Redirect, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectIdToken } from "../auth/redux";
+import { selectIsAuthenticated } from "../auth/redux";
 import { FormWrapper, Header, Logo, StyledLoginPage } from "./elements";
 import { Text } from "../../components/elements";
 
 function LoginPage() {
-  const idToken = useSelector((state: RootState) => selectIdToken(state.auth));
+  const isAuthenticated = useSelector((state: RootState) =>
+    selectIsAuthenticated(state.auth)
+  );
   const location = useLocation<{ from: { pathname: string } }>();
   const { from } = location.state || { from: { pathname: "/" } };
 
-  return !idToken ? (
+  return isAuthenticated ? (
+    <Redirect
+      to={{
+        pathname: from.pathname,
+        state: { from },
+      }}
+    />
+  ) : (
     <StyledLoginPage>
       <FormWrapper>
         <Header>
@@ -21,13 +30,6 @@ function LoginPage() {
         <LoginButton />
       </FormWrapper>
     </StyledLoginPage>
-  ) : (
-    <Redirect
-      to={{
-        pathname: from.pathname,
-        state: { from },
-      }}
-    />
   );
 }
 
